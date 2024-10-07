f147bc5e9fbe2d72a55f053bf8c3893b05677e86
SnowfallFox <icebornwhisker@gmail.com>
2024-10-07 12:50:20 +0000

    feat: added initial mongoose integration

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const mongoose Schema
+
+// let x = mongoose.model
 
 app.use(cors())
 app.use(express.static('public'))
