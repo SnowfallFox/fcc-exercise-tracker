4b9e44de01e76542482733e3ddc85f7135946fc5
SnowfallFox <icebornwhisker@gmail.com>
2024-10-08 10:48:30 +0000

    chore: very minor cleanup

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ require('dotenv').config()
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type:String, required:true },
   count: Number,
-  _id: { type: String, required: true },
   log: [{
     description: { type:String, required:true },
     duration: { type:Number, required:true },
